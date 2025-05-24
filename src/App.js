8dca50356d59d9cec77905ebdcc270aeaabfd2cb
Your Name <your.email@example.com>
2025-05-24 10:44:32 +0530

    my message

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import About from './pages/About';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path='/About' element={<About/>}/>
         </Routes>
       </div>
     </>
---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Hii im Musaib</h1>
+      <p>This is a social platform for developers to connect and grow together.</p>
+    </div>
+  );
+};
+
+export default About;
